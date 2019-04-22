9e2ea725ee374d2389c30225fb79411543dd5265
Ben Wiley <therealbenwiley@gmail.com>
2019-04-21 21:34:15 -0400

    Add quickstart example.

---
+++ example/quickstart/quickstart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// In a real app:
+// import { MediaPlayer } from 'win95-media-player';
+import { MediaPlayer } from '../..';
+
+const playlist = [
+  {
+    url: 'https://archive.org/download/CC1301_windows_95/CC1301_windows_95_512kb.mp4',
+    title: 'Computer Chronicles - Windows 95'
+  }
+];
+
+ReactDOM.render(
+  <MediaPlayer playlist={playlist} showVideo fullscreenEnabled />,
+  document.getElementById('app')
+);
